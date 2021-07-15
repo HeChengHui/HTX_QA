6d4c9d43ea133184c952a08562a7ec56fa46d593
HeChengHui <hechenghui97@hotmail.com>
2021-07-15 16:52:16 +0800

    Added Ranker functions (FARMRanker & SentenceTransformersRanker)

--- Haysatck/Haystack_QA.py
+++ Haysatck/Haystack_QA.py
@@ -74,6 +74,22 @@ document_store = ElasticsearchDocumentStore( # initialise the document store
 from haystack.retriever.sparse import ElasticsearchRetriever
 retriever = ElasticsearchRetriever(document_store=document_store)
 ## For this method, no need to update any embeddings.
+## Ranker -> SentenceTransformersRanker
+# from haystack.ranker import SentenceTransformersRanker
+# from haystack import Pipeline
+# ranker = SentenceTransformersRanker(model_name_or_path="cross-encoder/ms-marco-MiniLM-L-6-v2")
+# p = Pipeline()
+# p.add_node(component=retriever, name="ESRetriever", inputs=["Query"])
+# p.add_node(component=ranker, name="Ranker", inputs=["ESRetriever"])
+## Ranker -> FARMRanker
+from haystack.ranker import FARMRanker
+from haystack import Pipeline
+ranker = FARMRanker(model_name_or_path="nboost/pt-tinybert-msmarco", 
+                    num_processes=0, use_gpu=True)
+p = Pipeline()
+p.add_node(component=retriever, name="ESRetriever", inputs=["Query"])
+p.add_node(component=ranker, name="Ranker", inputs=["ESRetriever"])
+
 
 ## DPR method
 ## Use embedding models from huggingface. 
@@ -96,16 +112,15 @@ retriever = ElasticsearchRetriever(document_store=document_store)
 
 # # STEP 3: Initialise the Reader ------------------------------------------------------------------------------------------------------------------------------
 ## FARM method
-from haystack.reader.farm import FARMReader
-# reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=True)
-reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=True,
-                    num_processes=1, max_seq_len=512)
+# from haystack.reader.farm import FARMReader
+# reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=True,
+#                     num_processes=0, max_seq_len=512)
 
-## Tranformer method
-# from haystack.reader.transformers import TransformersReader
-# reader = TransformersReader(
-#     model_name_or_path="deepset/roberta-base-squad2", use_gpu=0, max_seq_len=512
-#     )
+## Tranformer method 
+from haystack.reader.transformers import TransformersReader
+reader = TransformersReader(
+    model_name_or_path="deepset/roberta-base-squad2", use_gpu=0, max_seq_len=512
+    )
 ## use_gpu <0, use CPU. Else GPU.
 ## max_seq_len default is 256, 512 seems to give better answers. 768 gives errors.
 # # END OF STEP 3 -------------------------------------------------------------------------------------------------------------------------------------------------
@@ -123,6 +138,6 @@ from datetime import datetime
 ## You can configure how many candidates the reader and retriever shall return
 ## The higher top_k_retriever, the better (but also the slower) your answers. 
 start = datetime.now()
-prediction = pipe.run(query="who made a speech at the official launch of HTX", top_k_retriever=5, top_k_reader=5)
+prediction = pipe.run(query="steps for PSHNs", top_k_retriever=5, top_k_reader=5)
 print_answers(prediction, details="all")
 print(datetime.now() - start) # print how long it takes to give the answer
